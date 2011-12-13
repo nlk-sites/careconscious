1fb774a4521ce38eef164bb35f2be0fd697d77d9
Aimee Broadbent <aimee@ninthlink.com>
2011-12-13 23:10:22 +0000

    Fixed the Assist page so that it displays only the to do items for the answers the user has selected in the asses tab.  Also changed it to only pull in the to dos for the most recent assess submission.

--- sites/all/modules/ADCI/aalter/pages/aalter_todos_page.tpl.php
+++ sites/all/modules/ADCI/aalter/pages/aalter_todos_page.tpl.php
@@ -1,21 +1,18 @@
-<?php if( !count($todos) ){ ?>
-  <p><?= t('No additional to-dos to display') ?></p>
-<?php } else { ?>
- <h2 class=principle-title><?= filter_xss($principle->p_title, $principle->p_format) ?></h2>
- <div id="task">
-   <ul>
-    <?php foreach ($todos as $todo) { ?>
-<li>
-  <?php
-    $title = substr($todo->title, 0, 50);
-		if (strlen($title) < strlen($todo->title)) $title = substr($title,0, strrpos($title, ' ')).' ...';
-  ?>
-  <div class="title"><?= $title ?></div>
-  <div class="description shadow">
-        <?= $todo->body ? check_markup($todo->body, $todo->format) : $todo->title ?>
-  </div>
-</li>
-    <?php } ?>
-   </ul>
- </div>
-<?php } ?>
+<div class="view-tasks">
+  <?php foreach ($todos as $todo) { ?>
+    <div class="views-row">
+      <?php
+      $title = substr($todo->title, 0, 50);
+	  if (strlen($title) < strlen($todo->title)) $title = substr($title,0, strrpos($title, ' ')).' ...';
+      ?>
+      <div class="views-field-title">
+        <span class="field-content"><?php print $title ?></span>
+      </div>
+      <div class="views-field-body">
+        <div class="field-content">
+          <?php print $todo->body ? check_markup($todo->body, $todo->format) : $todo->title; ?>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
+</div>
