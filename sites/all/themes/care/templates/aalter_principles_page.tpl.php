a9ab355eeec370b622440de11d3be144c0659eef
Troy Shields <troy@troyshields.com>
2011-12-14 21:32:51 +0000

    styles for user-tasks page

--- sites/all/themes/care/templates/aalter_principles_page.tpl.php
+++ sites/all/themes/care/templates/aalter_principles_page.tpl.php
@@ -1,11 +1,13 @@
 <?php if( !count($principles) ){ ?>
   <p><?php echo t('No additional to-dos to display') ?></p>
 <?php } else {?>
-  <div>These tasks act like a road map and will point you in the right direction through-out your caregiving journey. Print them out or come back to your user profile to review them anytime. Click on a Principle below to see the tasks associated with that Principle.</div>
+  <div class="mytasks-header">These tasks act like a road map and will point you in the right direction through-out your caregiving journey. Print them out or come back to your user profile to review them anytime. Click on a Principle below to see the tasks associated with that Principle.</div>
   <?php foreach ($principles as $group => $principle) {?>
-    <div class="tasks-lists">
-      <div class="tasks-principle">Principle <?php echo $principle->principle_number;?></div>
-      <h3 class="principle-title"><?php echo l($principle->p_title, 'dashboard/principle-'.$principle->principle_number, array('query' => 'quicktabs_dashboard_principle_'.$principle->principle_number.'_qt=2'))?></h3>
+    <div class="tasks-lists-main">
+      <div class="tasks-header">
+        <div class="tasks-principle">Principle <?php echo $principle->principle_number;?></div>
+        <h3 class="principle-title"><?php echo l($principle->p_title, 'dashboard/principle-'.$principle->principle_number, array('query' => 'quicktabs_dashboard_principle_'.$principle->principle_number.'_qt=2'))?></h3>
+      </div>
       <ul>
         <?php if(isset($todos[$principle->p_nid]) && !empty($todos[$principle->p_nid])){
           $p_todos = $todos[$principle->p_nid];
@@ -22,7 +24,7 @@
           }
         }?>
       </ul>
-      <?php echo l('View All', 'dashboard/principle-'.$principle->principle_number, array('query' => 'quicktabs_dashboard_principle_'.$principle->principle_number.'_qt=2'))?>
+      <div class="more-link"><?php echo l('View All', 'dashboard/principle-'.$principle->principle_number, array('query' => 'quicktabs_dashboard_principle_'.$principle->principle_number.'_qt=2'))?></div>
     </div>
   <?php }
 } ?>
