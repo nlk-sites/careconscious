7f63933d45af0e98cd181880431764887e59be54
Aimee Broadbent <aimee@ninthlink.com>
2012-01-12 17:42:14 +0000

    [BugzId:4415] Added text to Assist pages, depending on whether the assessment has been taken or not

--- sites/all/themes/care/templates/aalter_todos_page.tpl.php
+++ sites/all/themes/care/templates/aalter_todos_page.tpl.php
@@ -1,11 +1,16 @@
 <div class="view-tasks">
-  <?php foreach ($todos as $todo) { ?>
-    <div class="views-row">
-      <div class="views-field-body">
-        <div class="field-content">
-          <?php print $todo->body ? $todo->body : $todo->title; ?>
+  <?php if(empty($todos)){?>
+    <p>To view your customized family caregiver's plan, please <a href="#" onClick="$('li.qtab-1 a').click(); return false;">complete the Assessment</a> for this principle.</p>
+  <?php }else{?>
+    <p>Based on your answers we've delivered a customized plan of action, recommended resources, and valuable service providers. It's what you need to do, what you need to do it, and who you need to help you on your journey, all in one place!</p>
+    <?php foreach ($todos as $todo) { ?>
+      <div class="views-row">
+        <div class="views-field-body">
+          <div class="field-content">
+            <?php print $todo->body ? $todo->body : $todo->title; ?>
+          </div>
         </div>
       </div>
-    </div>
-  <?php } ?>
+    <?php }
+  }?>
 </div>
--- sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
+++ sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
@@ -1,11 +1,17 @@
 <div class="view-tasks">
-  <?php foreach ($todos as $todo) { ?>
-    <div class="views-row">
-      <div class="views-field-body">
-        <div class="field-content">
-          <?php print $todo->body ? $todo->body : $todo->title; ?>
+  <?php if(empty($todos)){?>
+    <?php $cur_node = node_load(arg(1));?>
+    <p>To view your customized family caregiver's plan, please <?php echo l('complete the Assessment', $cur_node->path, array('absolute' => TRUE, 'query' => 'quicktabs_'.str_replace(array('/', '-'), '_', $cur_node->path).'_qt=1'));?> for this principle.</p>
+  <?php }else{?>
+    <p>Based on your answers we've delivered a customized plan of action, recommended resources, and valuable service providers. It's what you need to do, what you need to do it, and who you need to help you on your journey, all in one place!</p>
+    <?php foreach ($todos as $todo) { ?>
+      <div class="views-row">
+        <div class="views-field-body">
+          <div class="field-content">
+            <?php print $todo->body ? $todo->body : $todo->title; ?>
+          </div>
         </div>
       </div>
-    </div>
-  <?php } ?>
+    <?php }
+  }?>
 </div>
