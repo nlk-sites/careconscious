ec9ef260e725cbae5e0ac79381bec51c1f534254
Aimee Broadbent <aimee@ninthlink.com>
2012-01-12 17:20:22 +0000

    [BugzId:4416] Removed node titles from Assist pages

--- sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
+++ sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
@@ -1,13 +1,6 @@
 <div class="view-tasks">
   <?php foreach ($todos as $todo) { ?>
     <div class="views-row">
-      <?php
-      $title = substr($todo->title, 0, 50);
-	  if (strlen($title) < strlen($todo->title)) $title = substr($title,0, strrpos($title, ' ')).' ...';
-      ?>
-      <div class="views-field-title">
-        <span class="field-content"><?php print $title ?></span>
-      </div>
       <div class="views-field-body">
         <div class="field-content">
           <?php print $todo->body ? $todo->body : $todo->title; ?>
