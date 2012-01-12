f698840b14a5d775bfa3939b414df23d58389121
Aimee Broadbent <aimee@ninthlink.com>
2012-01-12 19:27:32 +0000

    [BugzId:4415] Made requested text changes

--- sites/all/themes/care/templates/aalter_todos_page.tpl.php
+++ sites/all/themes/care/templates/aalter_todos_page.tpl.php
@@ -1,8 +1,13 @@
 <div class="view-tasks">
   <?php if(empty($todos)){?>
-    <p>To view your customized family caregiver's plan, please <a href="#" onClick="$('li.qtab-1 a').click(); return false;">complete the Assessment</a> for this principle.</p>
+    <p>To receive your customized family caregiver's plan, you must <a href="#" onClick="$('li.qtab-1 a').click(); return false;">complete the Assessment questions</a> for this principle. Go to the assessment tab above.</p>
   <?php }else{?>
-    <p>Based on your answers we've delivered a customized plan of action, recommended resources, and valuable service providers. It's what you need to do, what you need to do it, and who you need to help you on your journey, all in one place!</p>
+    <?php $cur_node = node_load(arg(1));
+    $principle_num = node_load($cur_node->field_principle_nid[0]['nid'])->field_principle_number[0]['value'];?>
+    <p><b>Your Family Caregiver's Plan for Principle <?php echo $principle_num;?></b></p>
+    <p>
+      Based on your answers we've delivered your customized Family Caregiver's Plan for <?php echo $cur_node->title; ?>, which includes the important tasks to complete and recommended advice and resources to help you complete those tasks. It's what you need to do and what you need to do it, all in one place.  
+    </p>
     <?php foreach ($todos as $todo) { ?>
       <div class="views-row">
         <div class="views-field-body">
--- sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
+++ sites/all/themes/care/templates/aalter_todos_pdf_page.tpl.php
@@ -1,9 +1,13 @@
 <div class="view-tasks">
+  <?php $cur_node = node_load(arg(1));?>
   <?php if(empty($todos)){?>
-    <?php $cur_node = node_load(arg(1));?>
-    <p>To view your customized family caregiver's plan, please <?php echo l('complete the Assessment', $cur_node->path, array('absolute' => TRUE, 'query' => 'quicktabs_'.str_replace(array('/', '-'), '_', $cur_node->path).'_qt=1'));?> for this principle.</p>
+    <p>To receive your customized family caregiver's plan, you must <?php echo l('complete the Assessment questions', $cur_node->path, array('absolute' => TRUE, 'query' => 'quicktabs_'.str_replace(array('/', '-'), '_', $cur_node->path).'_qt=1'));?>  for this principle. Go to the assessment tab above.</p>
   <?php }else{?>
-    <p>Based on your answers we've delivered a customized plan of action, recommended resources, and valuable service providers. It's what you need to do, what you need to do it, and who you need to help you on your journey, all in one place!</p>
+    <?php $principle_num = node_load($cur_node->field_principle_nid[0]['nid'])->field_principle_number[0]['value'];?>
+    <p><b>Your Family Caregiver's Plan for Principle <?php echo $principle_num;?></b></p>
+    <p>
+      Based on your answers we've delivered your customized Family Caregiver's Plan for <?php echo $cur_node->title; ?>, which includes the important tasks to complete and recommended advice and resources to help you complete those tasks. It's what you need to do and what you need to do it, all in one place.  
+    </p>
     <?php foreach ($todos as $todo) { ?>
       <div class="views-row">
         <div class="views-field-body">
