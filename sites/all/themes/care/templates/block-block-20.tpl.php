89a0679884fe87491f33ff1962012671534fc604
alex chousmith <alex@ninthlink.com>
2012-01-13 20:12:46 +0000

    [BugzId:4387] added checkboxes for Dashboard block

--- sites/all/themes/care/templates/block-block-20.tpl.php
+++ sites/all/themes/care/templates/block-block-20.tpl.php
@@ -9,62 +9,26 @@
     <?php //print $block->content ?>
     <h3 class="menu-title">Access the 8 Principles</h3>
 <ul class="menu">
-  <li class="item_principle1"><a href="/dashboard/principle-1">Learn the Landscape</a>
+<?php
+
+$ourprinciples = array('Learn the Landscape','Maintain Your Own Well Being','Manage Family Dynamics','Address Safety Issues','Recognize Residential Needs','Understand Legal and Financial Options','Help With Healthcare','Plan for Your Own Care');
+
+$princ_node_ids = array(244,245,246,247,248,249,498,499);
+
+foreach ($ourprinciples as $k => $v ) {
+	$i = $k+1;
+	?>
+  <li class="item_principle<?php print $i ?>"><?php echo l($v,'dashboard/principle-'. $i); ?>
     <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-1?quicktabs_dashboard_principle_1_qt=0#quicktabs-dashboard_principle_1_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-1?quicktabs_dashboard_principle_1_qt=1#quicktabs-dashboard_principle_1_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-1?quicktabs_dashboard_principle_1_qt=2#quicktabs-dashboard_principle_1_qt">Assist</a></li>
+      <li class="icon_learn"><a href="/dashboard/principle-<?php print $i ?>?quicktabs_dashboard_principle_<?php print $i ?>_qt=0#quicktabs-dashboard_principle_<?php print $i ?>_qt">Learn</a></li>
+      <li class="icon_assess"><a href="/dashboard/principle-<?php print $i ?>?quicktabs_dashboard_principle_<?php print $i ?>_qt=1#quicktabs-dashboard_principle_<?php print $i ?>_qt">Assess</a></li>
+      <li class="icon_assist"><a href="/dashboard/principle-<?php print $i ?>?quicktabs_dashboard_principle_<?php print $i ?>_qt=2#quicktabs-dashboard_principle_<?php print $i ?>_qt">Assist</a></li>
     </ul>
+    <?php
+	print '<span class="'. (aalter_user_todos($princ_node_ids[$k]) ? '' :'no') .'check"></span>';
+	?>
   </li>
-  <li class="item_principle2"><a href="/dashboard/principle-2">Maintain Your Own Well Being</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-2?quicktabs_dashboard_principle_2_qt=0#quicktabs-dashboard_principle_2_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-2?quicktabs_dashboard_principle_2_qt=1#quicktabs-dashboard_principle_2_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-2?quicktabs_dashboard_principle_2_qt=2#quicktabs-dashboard_principle_2_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle3"><a href="/dashboard/principle-3">Manage Family Dynamics</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-3?quicktabs_dashboard_principle_3_qt=0#quicktabs-dashboard_principle_3_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-3?quicktabs_dashboard_principle_3_qt=1#quicktabs-dashboard_principle_3_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-3?quicktabs_dashboard_principle_3_qt=2#quicktabs-dashboard_principle_3_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle4"><a href="/dashboard/principle-4">Address Safety Issues</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-4?quicktabs_dashboard_principle_4_qt=0#quicktabs-dashboard_principle_4_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-4?quicktabs_dashboard_principle_4_qt=1#quicktabs-dashboard_principle_4_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-4?quicktabs_dashboard_principle_4_qt=2#quicktabs-dashboard_principle_4_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle5"><a href="/dashboard/principle-5">Recognize Residential Needs</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-5?quicktabs_dashboard_principle_5_qt=0#quicktabs-dashboard_principle_5_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-5?quicktabs_dashboard_principle_5_qt=1#quicktabs-dashboard_principle_5_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-5?quicktabs_dashboard_principle_5_qt=2#quicktabs-dashboard_principle_5_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle6"><a href="/dashboard/principle-6">Understand Legal and Financial Options</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-6?quicktabs_dashboard_principle_6_qt=0#quicktabs-dashboard_principle_6_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-6?quicktabs_dashboard_principle_6_qt=1#quicktabs-dashboard_principle_6_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-6?quicktabs_dashboard_principle_6_qt=2#quicktabs-dashboard_principle_6_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle7"><a href="/dashboard/principle-7">Help With Healthcare</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-7?quicktabs_dashboard_principle_7_qt=0#quicktabs-dashboard_principle_7_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-7?quicktabs_dashboard_principle_7_qt=1#quicktabs-dashboard_principle_7_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-7?quicktabs_dashboard_principle_7_qt=2#quicktabs-dashboard_principle_7_qt">Assist</a></li>
-    </ul>
-  </li>
-  <li class="item_principle8"><a href="/dashboard/principle-8">Plan for Your Own Care</a>
-    <ul>
-      <li class="icon_learn"><a href="/dashboard/principle-8?quicktabs_dashboard_principle_8_qt=0#quicktabs-dashboard_principle_8_qt">Learn</a></li>
-      <li class="icon_assess"><a href="/dashboard/principle-8?quicktabs_dashboard_principle_8_qt=1#quicktabs-dashboard_principle_8_qt">Assess</a></li>
-      <li class="icon_assist"><a href="/dashboard/principle-8?quicktabs_dashboard_principle_8_qt=2#quicktabs-dashboard_principle_8_qt">Assist</a></li>
-    </ul>
-  </li>
+<?php } ?>
 </ul>
     <div class="clear"></div>
   </div>
