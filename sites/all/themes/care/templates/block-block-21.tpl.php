ab723fb46c0546e591b2950d607987c0e72a308c
alex chousmith <alex@ninthlink.com>
2012-01-26 18:59:00 +0000

    [BugzId:4469] "Review and Print your Family Caregiver's Plan"

--- sites/all/themes/care/templates/block-block-21.tpl.php
+++ sites/all/themes/care/templates/block-block-21.tpl.php
@@ -13,7 +13,7 @@ Watch the Video</li>
 <li class="step"><span class="stepnum">2</span><b>Step Two: Assess</b>
 Complete the Assessment</li>
 <li class="step"><span class="stepnum">3</span><b>Step Three: Assist</b>
-Review &amp; Print your Assist Plan</li>
+Review and Print your Family Caregiver's Plan</li>
 </ul>
 <div class='btn_todo'><?php echo l('View Dashboard', 'dashboard');?></div>
 <div class='btn_todo next'><?php echo l('Start Next Principle', 'dashboard/principle-3');?></div>
